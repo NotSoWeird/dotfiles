7903363cf893dc37a47177fbe08c85b5c677d9f2
NotSoWeird <wiktor@liew.se>
2023-09-09 12:12:47 +0200

    configuring hyprland

--- .config/sway/scripts/lockman.sh
+++ .config/sway/scripts/lockman.sh
@@ -4,6 +4,6 @@ swayidle \
     timeout  300 'swaymsg "output * power off"' \
     resume 'swaymsg "output * power on"' &
 # Locks the screen immediately
-swaylock
+swaylock --screenshots --clock --indicator-idle-visible --effect-blur 8x2 --effect-pixelate 10 --grace 3
 # Kills last background task so idle timer doesn't keep running
 kill %%
